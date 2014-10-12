1d986eabe4bfc7851f121c69cd91527e5023e523
Maya <maya@jenkins-mac-mini.local>
2014-10-12 11:41:14 +0300

    Initial Commit

---
+++ sintax-research/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sintax-research
+//
+//  Created by Maya on 10/12/14.
+//  Copyright (c) 2014 Maya. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
